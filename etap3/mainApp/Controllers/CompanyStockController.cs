fe3f51d44f5a5f528e14845872113b72baf80a12
Hubert Damaszko <259300@student.pwr.edu.pl>
2023-06-12 21:09:08 +0200

    Python YahooFinance data download

--- etap3/mainApp/Controllers/CompanyStockController.cs
+++ etap3/mainApp/Controllers/CompanyStockController.cs
@@ -3,6 +3,7 @@ using Platformy_Programowania_1.Models;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
 
 namespace Platformy_Programowania_1.Controllers;
 
@@ -17,64 +18,70 @@ public class CompanyStockController : Controller
     [HttpPost]
     public async Task<IActionResult> CreateStock(string companyName)
     {
-        var failedReguest = true;
-        var client = new HttpClient();
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/stock-time-series?symbol=AAPL%3ANASDAQ&period=1Y&language=en"),
-            Headers =
-            {
-                { "X-RapidAPI-Key", "b63aabfcf3mshebfdb3cb666b162p13b6d7jsn72e2c538fe72" },
-                { "X-RapidAPI-Host", "real-time-finance-data.p.rapidapi.com" },
-            },
-        };
-        using (var response = await client.SendAsync(request))
-        {
-            response.EnsureSuccessStatusCode();
-            var body = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(body);
-            var companyStock = JsonConvert.DeserializeObject<CompanyStock>(body);
+        string pythonScript = "wwwroot\\scripts\\StockAPI.py";
+        string arguments = "2023-06-08"; // data startowa
 
-            var newStock = companyStock?.Data?.Stock?.FirstOrDefault<Stock>() ?? throw new NullReferenceException();
-            Console.WriteLine(newStock);
-        }
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = "python";
+        startInfo.Arguments = $"{pythonScript} {arguments}";
+        startInfo.RedirectStandardOutput = true;
+        startInfo.UseShellExecute = false;
 
-        try
-        {
-            var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStringAsync();
+        Process process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
 
-            var companyStock = JsonConvert.DeserializeObject<CompanyStock>(responseBody);
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
 
-            var newStock = companyStock?.Data?.Stock?.FirstOrDefault<Stock>() ?? throw new NullReferenceException();
-
-            using (var db = new AppDbContext())
-            {
-                _context.Stocks.Add(newStock);
-                _context.SaveChanges();
-            }
-
-            failedReguest = false;
-        }
-        catch(HttpRequestException ex)
-        {
-            Console.WriteLine($"Request failed with error: {ex.Message}");  
-
-        }
-        catch(JsonSerializationException ex)
-        {
-            Console.WriteLine($"JSON deserialization failed with error: {ex.Message}");
-        }
-        catch(NullReferenceException ex)
-        {            
-            Console.WriteLine($"Stock data was not succesfully collected: {ex.Message}");  
-        }
-
-        if(failedReguest)
-            TempData["FailedRequest"] = "Failed to receive data. Company name could be incorrect. Please check and try again";
+        Console.WriteLine(output);
         
+        //using (var response = await client.SendAsync(request))
+        //{
+        //    response.EnsureSuccessStatusCode();
+        //    var body = await response.Content.ReadAsStringAsync();
+        //    Console.WriteLine(body);
+        //    var companyStock = JsonConvert.DeserializeObject<CompanyStock>(body);
+
+        //    var newStock = companyStock?.Data?.Stock?.FirstOrDefault<Stock>() ?? throw new NullReferenceException();
+        //    Console.WriteLine(newStock);
+        //}
+
+        //try
+        //{
+        //    var response = await client.SendAsync(request);
+        //    response.EnsureSuccessStatusCode();
+        //    var responseBody = await response.Content.ReadAsStringAsync();
+
+        //    var companyStock = JsonConvert.DeserializeObject<CompanyStock>(responseBody);
+
+        //    var newStock = companyStock?.Data?.Stock?.FirstOrDefault<Stock>() ?? throw new NullReferenceException();
+
+        //    using (var db = new AppDbContext())
+        //    {
+        //        _context.Stocks.Add(newStock);
+        //        _context.SaveChanges();
+        //    }
+
+        //    failedReguest = false;
+        //}
+        //catch(HttpRequestException ex)
+        //{
+        //    Console.WriteLine($"Request failed with error: {ex.Message}");  
+
+        //}
+        //catch(JsonSerializationException ex)
+        //{
+        //    Console.WriteLine($"JSON deserialization failed with error: {ex.Message}");
+        //}
+        //catch(NullReferenceException ex)
+        //{            
+        //    Console.WriteLine($"Stock data was not succesfully collected: {ex.Message}");  
+        //}
+
+        //if(failedReguest)
+        //    TempData["FailedRequest"] = "Failed to receive data. Company name could be incorrect. Please check and try again";
+
         return RedirectToAction("Index");
     }
 
