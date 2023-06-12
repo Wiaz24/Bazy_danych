6679d95c81d64bf7ccc2d4fd9451621ebd9011a0
Hubert Damaszko <259300@student.pwr.edu.pl>
2023-06-12 21:09:08 +0200

    Python YahooFinance data download

---
+++ etap3/mainApp/wwwroot/scripts/StockAPI.py
@@ -0,0 +1,9 @@
+﻿import yfinance as yf
+import pandas as pd
+import sys
+
+# Pobierz argumenty przekazane do skryptu
+arguments = sys.argv
+start = str(arguments[1])
+df = yf.download('AAPL',start = start,progress=False)
+print(df["Close"].iloc[0])
