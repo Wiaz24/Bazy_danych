48bbbf3651324e3e71ad4405ccc395a2693314b7
Jacek Czyz <259265@student.pwr.edu.pl>
2023-06-13 20:55:03 +0200

    crud daily

--- etap3/mainApp/AppDbContext.cs
+++ etap3/mainApp/AppDbContext.cs
@@ -9,9 +9,9 @@ namespace Platformy_Programowania_1
     public class AppDbContext : IdentityDbContext<User>
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Stock> Stocks { get; set; }
+        //public DbSet<Stock> Stocks { get; set; }
         public DbSet<Company> Companies { get; set; }
-        //public DbSet<DailyData> Daily { get; set; }
+        public DbSet<DailyData> Daily { get; set; }
         //public DbSet<YearlyData> Yearly { get; set; }
         //public DbSet<Favourite> Favourites { get; set; }
         public DbSet<Order> Orders { get; set; }
---
+++ etap3/mainApp/Services/Interfaces/IDailyService.cs
@@ -0,0 +1,15 @@
+﻿using Platformy_Programowania_1.Models;
+
+namespace Platformy_Programowania_1.Services.Interfaces
+{
+    public class IDailyService
+    {
+            Task<int> CreateDaily(DailyData daily);
+            Task<int> UpdateDaily(DailyData daily);
+            Task<int> DeleteDaily(int? id);
+            DailyData GetDailyById(int id);
+            Task<IEnumerable<DailyData>> GetDailys();
+        }
+
+}
+
