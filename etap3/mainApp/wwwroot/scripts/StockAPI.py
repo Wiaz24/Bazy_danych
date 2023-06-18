c0f6fba7eb73dd4b7e8a0dc3810150a61b7a6e44
Jacek Czyz <259265@student.pwr.edu.pl>
2023-06-18 16:33:17 +0200

    skrypt poprawki

--- etap3/mainApp/wwwroot/scripts/StockAPI.py
+++ etap3/mainApp/wwwroot/scripts/StockAPI.py
@@ -2,8 +2,35 @@
 import pandas as pd
 import sys
 
-# Pobierz argumenty przekazane do skryptu
+
+# Pobierz argumenty przekazane do skryptu       kiedy get yearly data to nazwa, data start, false. kiedy daily to data dzisiaj i false - da ostatni wpis
+#arguments = ["Tesla", "2023-06-15", True, True] #Company name, date, if hourly data, if only last data
 arguments = sys.argv
 start = str(arguments[1])
-df = yf.download('AAPL',start = start,progress=False)
-print(df["Close"].iloc[0])
+hourly_interval = arguments[2]
+only_last = arguments[3]
+
+if hourly_interval:
+    interval="1h"
+else:
+    interval="1d"
+
+match arguments[0]:
+    case "Apple":
+        com_symbol = 'AAPL'
+    case "Tesla":
+        com_symbol = 'TSL'
+    case "Adobe":
+        com_symbol = 'ADBE'
+    case "Meta Platforms":
+        com_symbol = 'META'
+
+df = yf.download(com_symbol, start = start, progress=False, interval=interval)
+close_vals = df["Close"]
+
+if only_last:
+    return_val = close_vals[len(close_vals)-1]      #newest data
+    sys.stdout.write(str(return_val))
+else:
+    for line in close_vals:                         #all data
+        sys.stdout.write(str(line))
