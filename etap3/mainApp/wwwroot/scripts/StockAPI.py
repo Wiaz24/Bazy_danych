de27a534e21df3217741845fd94ee684b372c2d8
Jacek Czyz <259265@student.pwr.edu.pl>
2023-06-18 23:02:08 +0200

    I wanna die :3

--- etap3/mainApp/wwwroot/scripts/StockAPI.py
+++ etap3/mainApp/wwwroot/scripts/StockAPI.py
@@ -6,39 +6,42 @@ import sys
 # Pobierz argumenty przekazane do skryptu       kiedy get yearly data to nazwa, data start, false. kiedy daily to data dzisiaj i false - da ostatni wpis
 #arguments = ["Tesla", "2023-06-15", True, True] #Company name, date, if hourly data, if only last data
 arguments = sys.argv
-start = str(arguments[1])
-hourly_interval = arguments[2]
-only_last = arguments[3]
-com_symbol="TSL"
-start = "2023-06-15"
+#start = str(arguments[2])
+hourly_interval = arguments[3]
+only_last = arguments[4]
+name = str(arguments[1]).strip()  # Strip the whitespace from the company name
+start="2023-06-15"
 if hourly_interval:
-    interval="1h"
+    interval = "1h"
 else:
-    interval="1d"
+    interval = "1d"
 
-match arguments[0]:
-    case "Apple":
-        com_symbol = 'AAPL'
-    case "Tesla":
-        com_symbol = 'TSL'
-    case "Adobe":
-        com_symbol = 'ADBE'
-    case "Meta Platforms":
-        com_symbol = 'META'
+com_symbol = ""
+if name == "Apple":
+    com_symbol = "AAPL"
+elif name == "Tesla":
+    com_symbol = "TSL"
+elif name == "Adobe":
+    com_symbol = "ADBE"
+elif name == "Meta Platforms":
+    com_symbol = "META"
+else:
+    sys.stderr.write("Invalid company name")
+    sys.exit(1)
 
-df = yf.download(com_symbol, start = start, progress=False, interval=interval)
+if not com_symbol:
+    sys.stderr.write("Invalid company name")
+    sys.exit(1)
+
+
+df = yf.download(com_symbol, start=start, progress=False, interval=interval)
 close_vals = df["Close"]
 
-
-
 if only_last:
-    return_val = close_vals[len(close_vals)-1]      #newest data
-    print(return_val)
-    #sys.stdout.write(str(return_val))
+    return_val = close_vals[len(close_vals) - 1]  # newest data
+    sys.stdout.write(str(return_val))
 else:
-    for line in close_vals:                         #all data
-        print(line)
-        #sys.stdout.write(str(line))
-
+    for line in close_vals:  # all data
+        sys.stdout.write(str(line))
 
 sys.stdout.flush()
